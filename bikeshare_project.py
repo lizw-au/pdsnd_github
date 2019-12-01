748120dcee78c040456dd2b38f263ee9163fe0a4
Liz Williams <mslizwilliams@gmail.com>
2019-12-01 12:24:43 +1100

    docs: update US spelling of 'analyze' to Australian spelling 'analyse'

--- bikeshare_project.py
+++ bikeshare_project.py
@@ -10,10 +10,10 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month, and day to analyse.
 
     Returns:
-        (str) city - name of the city to analyze
+        (str) city - name of the city to analyse
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
@@ -48,7 +48,7 @@ def load_data(city, month, day):
     Loads data for the specified city and filters by month and day if applicable.
 
     Args:
-        (str) city - name of the city to analyze
+        (str) city - name of the city to analyse
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
